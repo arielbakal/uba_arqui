c706fda6316964563f255a5eaa8501845e005496
arielbakal <arielbakal.dev@gmail.com>
2025-04-16 13:44:40 -0300

    Guia C-Av: until ej4

--- guias/C Avanzado/ej2.c
+++ guias/C Avanzado/ej2.c
@@ -7,19 +7,18 @@ typedef struct {
     double defensa;
 } monstruo_t;
 
-int main() {
-    monstruo_t evolution(monstruo_t monstruo) {
-        monstruo.ataque += 10;
-        monstruo.defensa += 10;
-        return monstruo;
-    };
+monstruo_t evolution(monstruo_t monstruo) {
+    monstruo.ataque += 10;
+    monstruo.defensa += 10;
+    return monstruo;
+}
 
+int main() {
     monstruo_t cthulhu = {"Cthulhu", 1000, 300, 10};
     printf("%s (Ataque: %f, Defensa: %f)\n", cthulhu.nombre, cthulhu.ataque, cthulhu.defensa);
 
-    monstruo_t cthulhu_evoluted = evolution(cthulhu);
-    printf("%s (Ataque: %f, Defensa: %f)\n", cthulhu_evoluted.nombre, cthulhu_evoluted.ataque, cthulhu_evoluted.defensa);
-
+    monstruo_t cthulhu_evolved = evolution(cthulhu);
+    printf("%s (Ataque: %f, Defensa: %f)\n", cthulhu_evolved.nombre, cthulhu_evolved.ataque, cthulhu_evolved.defensa);
 
     return 0;
 }
